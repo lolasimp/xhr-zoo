4432ff9da219f3893bd61f8d2c586f6908243116
Lola Simpson <lolalsimp@gmail.com>
2018-03-27 08:37:24 -0500

    added functions to list animal names

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-console.log(lola)
+
+const printToDom = (domString, divId) => {
+    document.getElementById(divId).innerHTML += domString;
+}
+
+function executeThisCodeifXHRFails(){
+    console.log("This is wrong");
+}
+
+const buildDomString = (theArray) => {
+    theArray.forEach((animals) => {
+    let domString = "";
+    domString += `<h1>${animals.name}</h1>`
+    printToDom(domString, "zoo")
+    })
+    
+};
+
+function executeThisCodeAfterFileLoaded(){
+    const data = JSON.parse(this.responseText);
+    buildDomString (data.animals);
+    }
+    const startApplication = () => {
+        let myRequest = new XMLHttpRequest();
+         myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
+         myRequest.addEventListener("error", executeThisCodeifXHRFails);
+         myRequest.open("GET", "animals.json");
+         myRequest.send();
+    }
+
+    startApplication();
